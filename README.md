a46c9edf17d68818531ef2e13447d417ad9fbdf6
kbz <kebingzao@gmail.com>
2016-11-09 14:54:22 +0800

    去掉多余的文件

--- README.md
+++ README.md
@@ -8,3 +8,29 @@ First, install `gulp-staticfy` as a development dependency:
 ```shell
 npm install --save-dev gulp-staticfy
 ```
+
+### Usage Examples
+
+```js
+gulp.task(`staticfy`, function() {
+        return gulp.src('src/**/*.html')
+            .pipe(staticfy({
+                query_string: `lang=en`
+            }))
+            .pipe(gulp.dest('build'));
+    });
+```
+
+### Options
+
+#### options.query_string
+Type: `String`
+Default value: `''`
+
+url query string
+
+#### options.server_url
+Type: `String`
+Default value: `''`
+
+the simple http server host
--- src/index.js
+++ src/index.js
@@ -9,8 +9,20 @@ var map = require('map-stream');
 module.exports = function(options) {
     var totalFileSize = 0;
     var finishFileSize = 0;
+    var ph;
+    var createPhPage = function(){
+        if(ph){
+            return ph.createPage();
+        }else{
+            return phantom.create().then(_ph => {
+                ph = _ph;
+                return ph.createPage();
+            })
+        }
+    };
     // close all http server
     var doAllFinish = () => {
+        ph.exit();
         SimpleServer.closeAll();
         console.log(gutil.colors.green("staticfy success"));
     };
@@ -21,46 +33,45 @@ module.exports = function(options) {
         }
     };
     return map(function (file, callback) {
-      if (file.isNull()) {
-          console.log("null");
-          return callback(null, file);
-      }
-      if (file.isStream()) {
-          console.log("stream");
-          return callback(null, file);
-      }
+        if (file.isNull()) {
+            console.log("null");
+            return callback(null, file);
+        }
+        if (file.isStream()) {
+            console.log("stream");
+            return callback(null, file);
+        }
 
-      if (file.isBuffer()) {
-          totalFileSize += 1;
-          var wwwDir = file.cwd;
-          var baseName = path.relative(file.cwd, file.path);
-          var url = 'http://localhost:{{port}}/';
-          var query_string = options.query_string ? `?${options.query_string}` : '';
-          SimpleServer.start(wwwDir, server => {
-              // Replace {{port}} with server.port
-              url = url.replace('{{port}}', server.port) + baseName + query_string;
-              var _ph, _page;
-              phantom.create().then(ph => {
-                  _ph = ph;
-                  return _ph.createPage();
-              }).then(page => {
-                  _page = page;
-                  return _page.open(url);
-              }).then(status => {
-                  //console.log(status);
-                  return _page.property('content')
-              }).then(content => {
-                  console.log(gutil.colors.blue(baseName) + gutil.colors.green(" load success"));
-                  doFinish();
-                  _page.close();
-                  _ph.exit();
-                  file.contents = new Buffer(content);
-                  callback(null, file);
-              }).catch(e => {
-                  doFinish();
-                  console.log(e)
-              });
-          });
-      }
+        if (file.isBuffer()) {
+            totalFileSize += 1;
+            // custom server url
+            var wwwDir = options.server_url || file.cwd;
+            var baseName = path.relative(wwwDir, file.path);
+            var url = 'http://localhost:{{port}}/';
+            var query_string = options.query_string ? `?${options.query_string}` : '';
+            console.log(gutil.colors.blue(`${baseName} loading ...`));
+            SimpleServer.start(wwwDir, server => {
+                // Replace {{port}} with server.port
+                url = url.replace('{{port}}', server.port) + baseName + query_string;
+                var ph_page;
+                createPhPage().then(page => {
+                    ph_page = page;
+                    return ph_page.open(url);
+                }).then(status => {
+                    //console.log(status);
+                    return ph_page.property('content')
+                }).then(content => {
+                    console.log(gutil.colors.blue(baseName) + gutil.colors.green(" load success"));
+                    ph_page.close();
+                    doFinish();
+                    file.contents = new Buffer(content);
+                    callback(null, file);
+                }).catch(e => {
+                    ph_page && ph_page.close();
+                    doFinish();
+                    console.log(e)
+                });
+            });
+        }
     });
 };
